16b67a92f3b778e5f5b8d176445f5bf9f7402858
joel-g <joelaguerra1@gmail.com>
2026-02-03 22:19:16 -0800

    fix typescript import

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState, ReactNode } from 'react';
+import { createContext, useContext, useEffect, useState, type ReactNode } from 'react';
 import { themes, type ThemeId, type Theme } from '../data/themes';
 
 interface ThemeContextType {
